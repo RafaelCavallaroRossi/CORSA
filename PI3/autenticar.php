ab7b0dc39ad52a68b46a1b64e815a22c6545fa52
R4finha_132 <160800920+RafaelCavallaroRossi@users.noreply.github.com>
2025-06-04 14:28:27 -0300

    Update2 autenticar.php
    Caso o Login não esteja funcionando para selecionar a categoria do usuário, tem que ligar a este código ou cola-lo nas linhas de PHP.

--- PI3/autenticar.php
+++ PI3/autenticar.php
@@ -1,37 +1,34 @@
 <?php
 session_start();
-require 'conexao.php'; 
+require 'conexao.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-
-    $stmt = $conn->prepare("SELECT id_usuario, nome_usuario, senha, tipo_usuario, rm FROM usuarios WHERE email = :email");
+    $stmt = $conn->prepare("SELECT id_usuario, nome_usuario, senha, tipo_usuario, rm FROM usuarios WHERE email = :email LIMIT 1");
     $stmt->bindParam(':email', $email);
     $stmt->execute();
     $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if ($usuario && password_verify($senha, $usuario['senha'])) { 
-   
+    if ($usuario && password_verify($senha, $usuario['senha'])) {
         $_SESSION['id_usuario'] = $usuario['id_usuario'];
         $_SESSION['nome_usuario'] = $usuario['nome_usuario'];
         $_SESSION['tipo_usuario'] = $usuario['tipo_usuario'];
         $_SESSION['rm'] = $usuario['rm'];
 
- 
-        if ($usuario['tipo_usuario'] == 'administrador') {
-            header('Location: admdash.html');
-        } elseif ($usuario['tipo_usuario'] == 'coordenador') {
-            header('Location: Coordenador/menucoordenador.php');
+        // Redirecionamento conforme o tipo de usuário
+        if ($usuario['tipo_usuario'] == 'Secretaria') {
+            header('Location: painel_secretaria.php');
+            exit;
         } elseif ($usuario['tipo_usuario'] == 'professor') {
             header('Location: Professor/menuprofessor.php');
-        } else {
+            exit;
+        }  else {
             header('Location: index.php');
+            exit;
         }
-        exit;
     } else {
-       
         $_SESSION['erro_login'] = 'Email ou senha inválidos.';
         header('Location: login.php');
         exit;

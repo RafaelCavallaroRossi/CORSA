06d10e3ea6349796e0745b2d807d343e6dc2690e
R4finha_132 <160800920+RafaelCavallaroRossi@users.noreply.github.com>
2025-06-14 16:46:41 -0300

    Update1 get_aulas.php
    Tem que atualizar também.

--- get_aulas.php
+++ get_aulas.php
@@ -1,6 +1,5 @@
 <?php
 include 'config.php';
-
 if (isset($_GET['turma_id'])) {
     $turma_id = $_GET['turma_id'];
     $stmt = $conn->prepare("SELECT * FROM Aulas WHERE turma_id = ?");
@@ -8,4 +7,3 @@ if (isset($_GET['turma_id'])) {
     $aulas = $stmt->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($aulas);
 }
-?>

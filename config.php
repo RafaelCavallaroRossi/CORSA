de2031f6a7026ce4ab03fad6893a543e911de783
R4finha_132 <160800920+RafaelCavallaroRossi@users.noreply.github.com>
2025-06-14 16:43:35 -0300

    Update1 config.php
    Tbm

--- config.php
+++ config.php
@@ -2,12 +2,10 @@
 $host = 'localhost';
 $db = 'InstituicaoVestibular';
 $user = 'root';
-$pass = ''; // ou a senha que você configurou
-
+$pass = '';
 try {
     $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
-?>

9375bf81c7ae866336cb5eb091b67524d96666e3
Luis Felipe Alves <luiselipe.alves@gmail.com>
2025-06-24 09:27:23 -0300

    Adicionado voltar para menu

--- cabecalho.php
+++ cabecalho.php
@@ -1,8 +1,20 @@
 <?php if (session_status() === PHP_SESSION_NONE) session_start(); ?>
 <header class="w-full bg-blue-900 text-white py-4 px-6 flex justify-between items-center shadow-md fixed top-0 left-0 z-10">
     <div class="flex items-center space-x-2">
-        <i class="fa-solid fa-school text-2xl"></i>
-        <span class="font-bold text-lg">Escolinha do...</span>
+        <?php
+            $menuLink = '#';
+            if (isset($_SESSION['tipo'])) {
+                if ($_SESSION['tipo'] === 'Secretaria') {
+                    $menuLink = 'menu.php';
+                } elseif ($_SESSION['tipo'] === 'Professor') {
+                    $menuLink = 'painel_professor.php';
+                }
+            }
+            ?>
+            <a href="<?php echo $menuLink; ?>" class="flex items-center space-x-2 hover:text-gray-300 transition">
+                <i class="fa-solid fa-school text-2xl"></i>
+                <span class="font-bold text-lg">Escolinha do...</span>
+            </a>
     </div>
     <div class="flex items-center space-x-4">
         <?php if (isset($_SESSION['usuario_id'])): ?>

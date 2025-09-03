199e09127e49b3c7585f59f11bc51bdc25facf8a
R4finha_132 <160800920+RafaelCavallaroRossi@users.noreply.github.com>
2025-09-03 11:32:45 -0300

    Delete get_aulas.php

--- get_aulas.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'config.php';
-if (isset($_GET['turma_id'])) {
-    $turma_id = $_GET['turma_id'];
-    $stmt = $conn->prepare("SELECT * FROM Aulas WHERE turma_id = ?");
-    $stmt->execute([$turma_id]);
-    $aulas = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($aulas);
-}
